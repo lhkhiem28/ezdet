c548caec424361e752945cb4757e293372bbb769
lhkhiem28 <lhkhiem28@gmail.com>
2022-11-08 07:52:37 +0000

    initial

--- source/libs.py
+++ source/libs.py
@@ -6,10 +6,11 @@ import pytorch_lightning; pytorch_lightning.seed_everything(22)
 from tqdm import tqdm
 
 import argparse
+import glob
 import pandas, numpy as np
 import cv2
 import torch
 import torch.nn as nn, torch.optim as optim
 import torch.nn.functional as F
-from pytorchyolo.models import *
-from pytorchyolo.utils.utils import *
+from pytorchyolo.models import Darknet
+from pytorchyolo.utils.loss import *; from pytorchyolo.utils.utils import *
---
+++ source/train.py
@@ -0,0 +1,35 @@
+
+import os, sys
+from libs import *
+from data import DetImageDataset
+
+datasets = {
+    "train":DetImageDataset(
+        images_path = "../datasets/VOC2007/train/images", labels_path = "../datasets/VOC2007/train/labels", 
+    ), 
+    "val":DetImageDataset(
+        images_path = "../datasets/VOC2007/train/images", labels_path = "../datasets/VOC2007/train/labels", 
+    ), 
+}
+train_loaders = {
+    "train":torch.utils.data.DataLoader(
+        datasets["train"], collate_fn = datasets["train"].collate_fn, 
+        num_workers = 8, batch_size = 8, 
+        shuffle = True
+    ), 
+    "val":torch.utils.data.DataLoader(
+        datasets["val"], collate_fn = datasets["val"].collate_fn, 
+        num_workers = 8, batch_size = 8, 
+        shuffle = True
+    ), 
+}
+model = Darknet("nets/yolov3.cfg")
+model.load_darknet_weights("nets/darknet53.conv.74")
+optimizer = optim.Adam(
+    model.parameters(), 
+    lr = model.hyperparams["lr"], weight_decay = model.hyperparams["weight_decay"], 
+)
+
+save_ckp_dir = "../ckps/VOC2007"
+if not os.path.exists(save_ckp_dir):
+    os.makedirs(save_ckp_dir)
